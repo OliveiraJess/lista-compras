c7c8150ef7e61cef3e69ec66aa51a2afd347382f
Flávio Nassim Bittar Filho <flavionassimbittarfilho@gmail.com>
2022-12-07 09:05:17 -0300

    logica inicial para a criação dos elementos HTML

--- index.js
+++ index.js
@@ -1,3 +1,94 @@
+
+
+
+
+
+// CREATE ITEM
+const creatItem = document.querySelector('#create_item')
+creatItem.addEventListener('click', () => {
+    const placeholder = creatItem.querySelector('p')
+    placeholder.setAttribute('activated','true')
+
+})
+
+
+
+
+
+
+// CREATE ITEM
+const item = (tag,item,checked=false) => {
+    const main = document.querySelector('main .container')
+    
+    const element = {
+        item,
+        tag,
+        checked
+
+    }
+
+    main.innerHTML += `<div class='item'>
+                            <div class='container'>
+                            <img src='style/image/grab.svg' alt='grab'>
+                            <input type='checkbox' class='field' id='checked' value='${element.checked}'>
+                            <div class='text'>
+                                <input type='text' value='${element.tag}' class='field' id='tag' disabled>
+                                <input type='text' value='${element.item}' class='field' id='order' disabled>
+                            </div>
+                            <button class='btn' id='pencil'></button>
+                            <button class='btn' id='trash'></button>
+                            </div>
+                        </div>`
+
+
+    return element
+
+}
+
+
+
+// Drop
+const drop = (placeholder,lista=[]) => {
+    const navegation = document.querySelector('#navigation #filters')
+    const element = {
+        placeholder,
+        listar: () => {
+            const tagLista = []
+            for(tag in lista) {
+                tagLista.push(`<li>${lista[tag]}</li>`)
+            }
+
+            return tagLista.toString().replace(/,/g, '')
+
+        }
+        
+    }
+
+    navegation.innerHTML += `<div class='drop_down' id='situation'>
+                                <div class='click'></div>
+                                <p activated='false'>${element.placeholder}</p>
+                                <button class='filter'><img activated='false' src='style/image/arrow_down.svg' alt='arrow'></button>
+
+                                <div options='close'>
+                                    <ul>
+                                    ${element.listar(lista)}
+                                    </ul>
+                                </div>
+                            </div>` 
+
+
+    
+}
+
+const a = item('vegetais','chuchu')
+
+const b = item('laticinios','leite')
+
+const c = item('bbbbbbbbbbbb','requeijao')
+
+drop('tag',[a.tag,b.tag,c.tag, 'ss', 'aaaa','joaaa'])
+
+
 // DROPS
 const drops = []
 document.querySelectorAll('.drop_down').forEach(drop => drops.push(drop))
@@ -54,16 +145,5 @@ drops.forEach(drop => {
 
 
     })
-
-})
-
-
-
-
-// CREATE ITME
-const creatItem = document.querySelector('#create_item')
-creatItem.addEventListener('click', () => {
-    const placeholder = creatItem.querySelector('p')
-    placeholder.setAttribute('activated','true')
 
 })
